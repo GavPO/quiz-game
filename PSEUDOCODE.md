7836fa7430bd9ab3095f94ac40cfa59d64b921fa
Gavin <gavinporlich@gmail.com>
2022-07-06 13:04:41 -0500

    HTML/JS Outline created/Initial Commit

---
+++ PSEUDOCODE.md
@@ -0,0 +1,13 @@
+# Step 1
+1. Link to highscores should be in html
+1. In the html there should be a container that I append the questions to.
+1. Timer should be in HTML
+1. When I click on start button run the code to start the quiz
+    1. Prevent Default so page does not refresh
+    1. Dynamically render the questions/answers to the screen
+    1. Answers should be button
+    1. Set max time on timer
+1. In the html there should be a container that we append the questions and answers to
+    1. Put the event listener on the container
+    1. If corrent show the next question
+    1. If incorrect decrease time. If time is 0 or no more questions, end the game
---
+++ assets/js/questions.js
@@ -0,0 +1,31 @@
+var questions = [
+    {
+        questionText: "How to decalre a variable in JavaScript",
+        choices: ["var", "while", "for", "if"],
+        answer: "var"
+    },
+
+    {
+        questionText: "How to decalre a variable in JavaScript",
+        choices: ["var", "while", "for", "if"],
+        answer: "var"
+    },
+
+    {
+        questionText: "How to decalre a variable in JavaScript",
+        choices: ["var", "while", "for", "if"],
+        answer: "var"
+    },
+
+    {
+        questionText: "How to decalre a variable in JavaScript",
+        choices: ["var", "while", "for", "if"],
+        answer: "var"
+    },
+
+    {
+        questionText: "How to decalre a variable in JavaScript",
+        choices: ["var", "while", "for", "if"],
+        answer: "var"
+    }
+]
